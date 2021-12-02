e07aca2627a6fd3cddce72a90e5927bfeb9c4851
GustavJoha <79506494+GustavJoha@users.noreply.github.com>
2021-12-02 15:04:56 +0100

    PEW PEW
    jag ger upp

---
+++ Assets/scripts/buttonlazer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonlazer : MonoBehaviour
+{
+    private RaycastHit buttonhit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out buttonhit, Mathf.Infinity, 1))
+        {
+                print("bruh");
+                print(buttonhit.transform);
+        }
+    }
+}
